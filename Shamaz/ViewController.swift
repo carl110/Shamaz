7fbd6d20f2144d4d33ced6c6039b49a69bad85f4
Carl Wainwright <carljame7@gmail.com>
2018-04-23 11:45:14 +0100

    Added Random Number generator function for next Player button

--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     //List of times used for Past and Future Prompts
         let time = ["1 day", "2 days", "3 days", "4 days" ,"5 days", "1 week", "2 weeks", "3 weeks", "1 month", "2 months", "3 months", "6 months", "1 year", "2 years", "3 years"]
     
+    //List of numbers for Players
+        let player = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
     @IBOutlet weak var pastFuturePrompt: UILabel!
     
     @IBAction func pastPrompt(_ sender: UIButton) {
@@ -49,5 +52,16 @@ class ViewController: UIViewController {
         pastFuturePrompt.text = futurePrompt
     }
     
+    @IBAction func nextPlayer(_ sender: UIButton) {
+        
+        //Generate random number for next Player
+        let randomPlayerIndex = Int(arc4random_uniform(UInt32(player.count)))
+        let randomPlayer = player[randomPlayerIndex]
+        
+        //Change text in 'pastFuturePromt' to show random Player number
+        let playerPrompt = "Please pass the phone to Player number " + randomPlayer + ". Once they have the phone they can choose 'What DID I do' or 'What WILL I do'"
+        
+        pastFuturePrompt.text = playerPrompt
+    }
 }
 
