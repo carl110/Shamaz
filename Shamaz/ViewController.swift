7284f1318e0c8664132df3ddce512731dd4d75a3
Carl Wainwright <carljame7@gmail.com>
2018-05-07 16:48:46 +0100

    Cleaned up and recorded code

--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -12,10 +12,7 @@ extension UIView {
     
     func roundCorners(for corners: UIRectCorner, cornerRadius: CGFloat)
     {
-        let path = UIBezierPath(roundedRect: self.bounds,
-                                byRoundingCorners: corners,
-                                cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
-        
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
         let maskLayer = CAShapeLayer()
         maskLayer.path = path.cgPath
         self.layer.mask = maskLayer
@@ -24,50 +21,37 @@ extension UIView {
     func setRadius(radius: CGFloat) {
         self.layer.cornerRadius = radius ;
         self.layer.masksToBounds = true;
-        
     }
 }
-
 extension UIButton {
     func centerTextHorizontally(spacing: CGFloat) {
         titleEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
         contentEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
-        
     }
 }
-
 class ViewController: UIViewController {
     
     let model = ShamazModelView()
     
-
     @IBOutlet weak var what: UILabel!
-    @IBOutlet weak var pastFuturePrompt: UILabel!
-    
-    
     @IBOutlet weak var pastPrompt: UIButton!
+    @IBOutlet weak var futurePrompt: UIButton!
+    @IBOutlet weak var pastFuturePrompt: UILabel!
+    @IBOutlet weak var nextPlayer: UIButton!
+
     @IBAction func pastPrompt(_ sender: UIButton)
     {
         //Change text in 'pastFuturePromt' to random past time
         pastFuturePrompt.text = "Describe what your day was like \(model.myPTime) ago, with as much detail as you can remember"
     }
-    
-
-    @IBOutlet weak var futurePrompt: UIButton!
     @IBAction func futurePrompt(_ sender: UIButton)
     {
         pastFuturePrompt.text = "Describe what you would like to be doing in \(model.myPTime), with as much detail as you can share."
     }
-
-    @IBOutlet weak var nextPlayer: UIButton!
     @IBAction func nextPlayer(_ sender: UIButton)
      {
         pastFuturePrompt.text = "Please pass the phone to Player number \(model.noPlayers)"
-        
-      
-        
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -81,13 +65,7 @@ class ViewController: UIViewController {
    
         //Center text using extension
         nextPlayer.centerTextHorizontally(spacing: 10)
-
-      //  nextPlayer.frame.size.width = 250;
-      // nextPlayer.titleLabel?.numberOfLines = 1
-        
-        
  }
-    
     override func viewDidLayoutSubviews() {
         
         //Use roundCorners extension to round button corners
@@ -95,14 +73,11 @@ class ViewController: UIViewController {
         pastPrompt.roundCorners(for: [.bottomLeft, .bottomRight], cornerRadius: 8)
         futurePrompt.roundCorners(for: [.bottomLeft, .bottomRight], cornerRadius: 8)
         
-          nextPlayer.frame.size.width = 150
+        nextPlayer.frame.size.width = 150
         nextPlayer.center.x = self.view.center.x
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
-
