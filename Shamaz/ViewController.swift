2abc8bdc38257c3a0e0b67f2c1171bc58c40aec6
Carl Wainwright <carljame7@gmail.com>
2018-05-07 14:00:55 +0100

    Added extension to centre text in UIButtons

--- Shamaz/ViewController.swift
+++ Shamaz/ViewController.swift
@@ -22,7 +22,13 @@ extension UIView {
     }
 }
 
-
+extension UIButton {
+    func centerText(spacing: CGFloat) {
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
+        
+    }
+}
 
 class ViewController: UIViewController {
     
@@ -39,47 +45,43 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var pastPrompt: UIButton!
-    @IBAction func pastPrompt(_ sender: UIButton) {
-        
-        
-        
+    @IBAction func pastPrompt(_ sender: UIButton)
+    {
         //Change text in 'pastFuturePromt' to random past time
-//
-
-
-        
         pastFuturePrompt.text = "Describe what your day was like \(model.myPTime) ago, with as much detail as you can remember"
-        
     }
     
 
     @IBOutlet weak var futurePrompt: UIButton!
-    @IBAction func futurePrompt(_ sender: UIButton) {
-
-       
+    @IBAction func futurePrompt(_ sender: UIButton)
+    {
         pastFuturePrompt.text = "Describe what you would like to be doing in \(model.myPTime), with as much detail as you can share."
     }
 
     @IBOutlet weak var nextPlayer: UIButton!
     @IBAction func nextPlayer(_ sender: UIButton)
      {
-
         pastFuturePrompt.text = "Please pass the phone to Player number \(model.noPlayers)"
         
-
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //call string from ShamazModelView
         model.temp()
         model.tempPlayer()
         
+        //Use roundCorners extension to round button corners
         nextPlayer.roundCorners([.allCorners], radius: 8)
         futurePrompt.roundCorners([.allCorners], radius: 8)
         pastPrompt.roundCorners([.allCorners], radius: 8)
- 
-    }
+   
+        //Center text using extension
+        nextPlayer.centerText(spacing: 10)
+
+ }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
