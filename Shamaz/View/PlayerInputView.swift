0741f4e66fe454f0a9167ed80cd76a1c97eafc7a
Carl Wainwright <carljame7@gmail.com>
2018-05-09 09:59:38 +0100

    Started creation of PlayerInputView to add player names to the game

---
+++ Shamaz/View/PlayerInputView.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerInputView.swift
+//  Shamaz
+//
+//  Created by Carl Wainwright on 09/05/2018.
+//  Copyright © 2018 Carl Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class PlayerInputView: UIViewController {
+ 
+    @IBOutlet weak var playerList: UILabel!
+    
+    
+    @IBAction func nameInput(_ sender: UITextField) {
+    }
+    
+    @IBAction func addPlayer(_ sender: UIButton) {
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+    }
+}
