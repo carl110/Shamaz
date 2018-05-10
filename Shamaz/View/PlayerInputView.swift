509d48a4380bfdb9f02ecbc0218c684f42304599
Carl Wainwright <carljame7@gmail.com>
2018-05-10 12:11:06 +0100

    Designed button to append text to UILabel

--- Shamaz/View/PlayerInputView.swift
+++ Shamaz/View/PlayerInputView.swift
@@ -11,14 +11,26 @@ import UIKit
 class PlayerInputView: UIViewController {
  
     @IBOutlet weak var playerList: UILabel!
-    
+    @IBOutlet weak var nameInput: UITextField!
+
+    var nameText = ""
+    var playerNumber = 0
     
     @IBAction func nameInput(_ sender: UITextField) {
     }
     
     @IBAction func addPlayer(_ sender: UIButton) {
+        
+        playerNumber += 1
+        nameText = nameInput.text!
+        playerList.text = (playerList.text ?? "") + "\(playerNumber). \(nameText)\n"
+        
     }
     
     @IBAction func startGame(_ sender: UIButton) {
     }
+    override func viewDidLoad() {
+        
+    }
 }
+
